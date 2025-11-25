47bc0559ef472e12d5fffb80bdae847ab1d0907c
thakurkaran007 <thakurkaran0345@example.com>
2025-11-25 21:54:58 +0530

    completed

--- apps/web/auth.config.ts
+++ apps/web/auth.config.ts
@@ -3,19 +3,9 @@ import type { NextAuthConfig } from "next-auth";
 import { LoginSchema } from "./schema";
 import { getUserByEmail } from "./data/user";
 import bcrypt from 'bcryptjs';
-import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
 
 const authConfig: NextAuthConfig = {
     providers: [
-        Github({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        }),
-        Google({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
-        }),
         Credentials({
             async authorize(credentials) {
                 const validation = LoginSchema.safeParse(credentials);
